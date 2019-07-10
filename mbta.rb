7d7aafb25b4538566a793f80c52f2ddeb097d285
Munira alshuhail <muniraalshuhail@Muniras-MacBook-Air.local>
2019-07-10 07:59:39 +0300

    Homeworkw4d3

--- mbta.rb
+++ mbta.rb
@@ -1,6 +1,36 @@
 # Your solution here
 class Subway
+
+  attr_accessor :stops , :lines
+
+def initialize (stops, start_line,end_line,end_station)
+  @stops, @start_line, @end_line, @end_station = stops, start_line, end_line, end_station
+
+  subway_line = {
+   red_line: ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"],
+   green_line: ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes","kenmore"],
+   orange_line: ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay"]
+ }
+ 
+ @redline = Line.new('red', red_line)
+ @greenline = Line.new('green', green_line)
+ @orangeline = Line.new('orange', orange_line)
+
+
+
+
+
+
+end
+
+
+
+
   def stops_between_stations(start_line, start_station, end_line, end_station)
+
+    if start_line == "red"
+
+  
   end
 end
   
